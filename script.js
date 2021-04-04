af45ef5d9b2bf53bebebb4d9f83f47a8df22551f
b0b3r <coolzyzop@mail.ru>
2021-04-04 14:03:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+var message = 'Hello!!! Yfrjytw-nj'; 
+
+var slotsArray = [];
+
+slotsArray[0] = {
+	toggle: true,
+	time:"00:00",
+	fixPayment: 0,
+	varPayment: 0,
+}
+
+slotsArray[1] = {
+	toggle: true,
+	time:"01:00",
+	fixPayment: 0,
+	varPayment: 0,
+}
+
+
+/*
+var allQuestions = new Array();
+    
+function loadQuestions() {
+    $.getJSON('question.json', function (data) {
+        allQuestions = data.quiz;
+    })
+    .error(function() {
+        console.log('error: JSON not loaded'); 
+    })
+    .done(function() {
+        console.log( "JSON loaded!" );
+        printQuestion(allQuestions[0].question); 
+    });
+}
+*/
+
+/*
+var JSONItems = []; 
+
+$.getJSON( "question.json", function(data){ 
+  JSONItems = data; 
+  console.log(JSONItems); 
+});
+*/
+
+//document.getElementById("_message").innerHTML = message;
+
+
+
+
+//console.log (slotsArray[0]);
+
